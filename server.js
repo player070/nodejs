c6e2efe80f5e1f6dc45ff82bfc7e3400c11c5c3e
qjin <qjin.web070@gmail.com>
2022-09-16 16:02:07 +0900

    express, nodemon 라이브러리 설치, get 요청으로 메세지와 파일 전송하기

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.listen(8080, function(){
+  console.log("listening on 8080")
+});
+
+app.get('/beauty',function(req,res){
+  res.send('뷰티용품점입니다!.');
+});
+
+app.get('/',function(req,res){
+  res.sendFile(__dirname + '/index.html');
+});
