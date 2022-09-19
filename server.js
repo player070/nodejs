5b1b322c04fcf9fd086d754087c229ab9b8b56e9
qjin <qjin.web070@gmail.com>
2022-09-19 14:18:25 +0900

    install mongoDB

--- server.js
+++ server.js
@@ -1,14 +1,34 @@
 const express = require('express');
 const app = express();
 
-app.listen(8080, function(){
-  console.log("listening on 8080")
-});
+const bodyParser = require('body-parser'); //서버요청을 쉽게 꺼내서 쓸 수 있는 라이브러리. 
+app.use(bodyParser.urlencoded({extended : true})); // ↑사용하기. 
 
-app.get('/beauty',function(req,res){
-  res.send('뷰티용품점입니다!.');
-});
+
+const MongoClient = require('mongodb').MongoClient;
+MongoClient.connect('mongodb+srv://qjin:Gorillaz-66@cluster0.xaphkkg.mongodb.net/?retryWrites=true&w=majority',function(에러, client){
+
+  app.listen(8080, function(){
+    console.log("listening on 8080")
+  });
+
+})
+
+
+
+
+
 
 app.get('/',function(req,res){
   res.sendFile(__dirname + '/index.html');
+}); 
+
+app.get('/write',function(req,res){
+  res.sendFile(__dirname + '/write.html');
 });
+
+app.post('/add', function(요청, 응답){
+  // 응답.send('전송완료');
+  console.log(요청.body)
+  응답.sendFile(__dirname + '/write.html')
+})
