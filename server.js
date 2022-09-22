4a049f59a8d6528953deea596a5ff0d784fdca89
qjin <qjin.web070@gmail.com>
2022-09-22 16:51:04 +0900

    프로필 삭제기능 추가

--- server.js
+++ server.js
@@ -18,36 +18,28 @@ app.get('/write',function(요청,응답){응답.sendFile(__dirname + '/write.htm
 
 
 
-app.post('/addProfile',function(요청,응답){
-  db.collection('counter').findOne({name:'회원수'},function(에러,결과){
-    var 회원수 = 결과.totalPost;
-    db.collection('posttest').insertOne({_id : 회원수 + 1 ,username : 요청.body.username, gender : 요청.body.gender, e_id : 요청.body.e_id, e_address : 요청.body.e_address},function(){
-      console.log('profile저장완료');
-
-      // counter라는 콜렉션에 있는 totalpost라는 항목도 1 증가시켜야한다.
-      db.collection('counter').updateOne({name:'회원수'},{ $inc : {totalPost:1}},function(에러, 결과){});
-    });    
-  });
-  응답.sendFile(__dirname + '/index.html')
-});
-
-
-// /write 를 통해 /addtodo에 post로 정보를 보낸다. 
-// id를 관리하는 collection명은 'counter', id는 'totalTodo', 식별자는 name : '할일수'
-// todo 정보를 입력하는 collection 명은 'post'
-// 보내는 내용 : 제목 : title, 날짜 : date
-// 정보를 보낸 뒤에 totalTodo의 값을 1 올려준다. 
-
-
-app.post('/addtodo',function(요청, 응답){
-  db.collection('counter').findOne({name : '할일수'},function(에러, 결과){
+app.post('/addtodo',function(요청,응답){
+  db.collection('counter').findOne({name : '할일수'},function(에러,결과){
     var 할일수 = 결과.totalTodo;
-    db.collection('post').insertOne({_id : 할일수 + 1 , 제목 : 요청.body.title, 날짜 : 요청.body.date},function(){
-      db.collection('counter').updateOne({name:'할일수'},{ $inc : {totalTodo : 1}},function(에러, 결과){});
+    db.collection('post').insertOne({_id : 할일수 + 1 , 제목 : 요청.body.title , 날짜 : 요청.body.date},function(에러,결과){
+      db.collection('counter').updateOne({name:'할일수'},{ $inc : {totalTodo : 1}} , function(에러,결과){})
     });
   });
   응답.sendFile(__dirname + '/write.html')
 });
+
+
+app.post('/addProfile',function(요청,응답){
+  db.collection('counter').findOne({name:'회원수'},function(에러,결과){
+    var 회원수 = 결과.totalUser;
+    db.collection('posttest').insertOne({_id : 회원수 + 1 , username : 요청.body.username, gender : 요청.body.gender, e_id : 요청.body.e_id, e_address : 요청.body.e_address},function(에러, 결과){
+      db.collection('counter').updateOne({name:'회원수'},{ $inc : {totalUser : 1}},function(에러, 결과){});
+    });
+  });
+  응답.sendFile(__dirname + '/signin.html');
+})
+
+
 
 
 app.get('/list',function(요청,응답){
@@ -75,6 +67,22 @@ app.delete('/deleteTodo',function(요청, 응답){
     }
   })
 })
+
+
+app.delete('/deleteUser',function(요청, 응답){
+  요청.body._id = parseInt(요청.body._id);
+  db.collection('posttest').deleteOne(요청.body, function(에러,결과){
+    if(에러){
+      응답.status(400).send({message:'실패'});
+    }else{
+      응답.status(200).send({message:'성공'});
+    }
+  })
+})
+
+
+
+
 
 app.get('/detail/:id',function(요청,응답){
   db.collection('post').findOne({_id : 1},function(에러, 결과){
